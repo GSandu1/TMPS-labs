02004beb5809981665816005075f940aa4db5335
GSandu1 <gazeasandu@gmail.com>
2023-09-25 21:28:33 +0300

    small mistake

--- lab1/domain/models/Review.java
+++ lab1/domain/models/Review.java
@@ -3,7 +3,7 @@ package domain.models;
 public class Review {
     private Customer reviewer;
     private String comment;
-    private int rating;  // Considered as stars.
+    private int rating;  //stars.
 
     public Review(Customer reviewer, String comment, int rating) {
         this.reviewer = reviewer;
